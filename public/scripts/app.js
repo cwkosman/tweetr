5355d3269740fd9a23a844b819d8c17f45bd33e0
cwkosman <cwkosman@gmail.com>
2017-02-23 21:26:27 +0000

    Documents, HTML and CSS support likes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,32 +28,36 @@ function timeSince(date) {
   return `${Math.floor(seconds / chosenInterval.seconds)} ${chosenInterval.unit}`;
 }
 
-function createTweetElement(database) {
+function createTweetElement(tweetdata) {
   //Destructuring in ES6
-  // const {name, avatars} = database.user
+  // const {name, avatars} = tweetdata.user
   //Split into seperate functions
   let $tweetImage = $('<img>', {
     "class": "tweet-avatar",
-    src: database.user.avatars.small
+    src: tweetdata.user.avatars.small
   });
   let $tweetPoster = $('<h2></h2>', {
     "class": "tweet-poster",
-    text: database.user.name
+    text: tweetdata.user.name
   });
   let $tweetHandle = $('<p></p>', {
     "class": "tweet-handle",
-    text: database.user.handle
+    text: tweetdata.user.handle
   });
   let $tweetHeader = $('<header class="tweet-header"></header>').append($tweetImage, $tweetPoster, $tweetHandle);
   let $tweetBody = $('<p></p>', {
     "class": "tweet-content",
-    text: database.content.text
+    text: tweetdata.content.text
   });
   let $tweetAge = $('<p></p>', {
     "class": "tweet-age",
-    text: `${timeSince(database.created_at)} ago`
+    text: `${timeSince(tweetdata.created_at)} ago`
   });
-  let $tweetIcons = $('<div class="tweet-icons">\n<span class="fa fa-flag"></span><span class="fa fa-retweet"> </span><span class="fa fa-heart"> </span></div>');
+  let $tweetIcons = $('<ul class="tweet-icons"></ul>');
+  let $iconShare = $('<li class="fa fa-share"></li>');
+  let $iconRetweet = $('<li class="fa fa-retweet"></li>');
+  let $iconLike = $(`<li class="fa fa-heart"></li><span class="tweet-likes">${tweetdata.likes}<span>`);
+  $tweetIcons.append($iconShare, $iconRetweet, $iconLike);
   let $tweetFooter = $('<footer class="tweet-footer"></footer>').append($tweetAge, $tweetIcons);
   return $('<article class="tweet"></article>').append($tweetHeader, $tweetBody, $tweetFooter);
 }
